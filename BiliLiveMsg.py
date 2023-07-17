639666e9c1c66e7ad2acf5ce3b1507a317709d53
Nanthan <49605891+NanthanYYDS@users.noreply.github.com>
2023-07-17 16:37:36 +0800

    Update BiliLiveMsg.py

--- BiliLiveMsg.py
+++ BiliLiveMsg.py
@@ -1,7 +1,9 @@
 import requests
 
 # 直播间ID
-roomID = '22884968'
+#-------------------------此处需修改---------------------------#
+roomID = ''
+#-------------------------此处需修改---------------------------#
 
 # 获取弹幕API
 url_get='http://api.live.bilibili.com/ajax/msg?roomid='
@@ -22,40 +24,18 @@ def get_msg():
     return text
 
 
-# 发送一条弹幕
+# 发送一条弹幕，返回bool值，成功为 1，反之为 0 
 def post_msg(content:str):
     # 先判一下内容是否非空
     if content == '':
         print('请指定发送弹幕内容')
         return False
+        
     # 初始化cookie
-    cookies = {
-        'buvid3': 'D2B57090-1F01-F8CB-8BCE-435D21361B3820473infoc',
-        'b_nut': '1689299220',
-        '_uuid': 'EB10764D6-C46F-451B-15110-610376C38C6F120682infoc',
-        'buvid4': '6172BA89-0611-05D6-0DF5-C01130BF306921236-023071409-FpRurigT5QQ9c7yecMklWA%3D%3D',
-        'rpdid': "|(J|YkJmm|JY0J'uY)mkmmkkR",
-        'header_theme_version': 'CLOSE',
-        'SESSDATA': '7f671553%2C1704855077%2C61f5b%2A72ckmwNfqzsRCOevRKzJyjh3xVJ9siUs1NW7gSG5R_K3_E_hDE8LyuuOucrANVn2bjXv3PcgAAAgA',
-        'bili_jct': '993436a7912b537cef424b331429ff9f',
-        'DedeUserID': '472975402',
-        'DedeUserID__ckMd5': '54709fd6c6cd4162',
-        'sid': '54srw0e6',
-        'FEED_LIVE_VERSION': 'V_LIVE_2',
-        'CURRENT_QUALITY': '80',
-        'fingerprint': '5c5915367ce67775efa9a9e2ac0bfd85',
-        'buvid_fp_plain': 'undefined',
-        'bsource': 'search_baidu',
-        'home_feed_column': '5',
-        'bp_t_offset_472975402': '818897366815866979',
-        'CURRENT_FNVAL': '4048',
-        'LIVE_BUVID': 'AUTO8916895366686212',
-        'b_lsid': 'C3FF8EDD_18962433F58',
-        'browser_resolution': '1707-809',
-        'buvid_fp': 'f56061f596cce5dc9f7ae2105acf280c',
-        '_dfcaptcha': 'ca51c788bd5c8fcef34e2208f6fa3d78',
-        'PVID': '25',
-    }
+    #-------------------------此处需修改---------------------------#
+    cookies = {}
+    #-------------------------此处需修改---------------------------#
+    
     # 初始化data
     data = {
         'bubble': '0',
@@ -84,8 +64,8 @@ if roomID == '':
     exit()
 print('1.获取弹幕\n2.发送弹幕')
 cmd= input()
-if cmd == '1':
 
+if cmd == '1':
     msg=''
     #不间断的运作，我这里就直接写死循环了，反正是测试代码
     while True:
